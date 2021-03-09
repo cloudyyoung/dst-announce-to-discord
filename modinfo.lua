9f210fe87a55302ab6c2fdc0c61e98da0c07bd62
Cloudy Young <cloudyyoung74@outlook.com>
2021-03-09 13:21:03 +0800

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,19 @@
+name = "Announce to Discord"
+description = 
+[[
+Announce to Discord
+]]
+author = "Cloudy Young"
+version = "0.0.1"
+forumthread = ""
+api_version = 10
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+dst_compatible = true
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+all_clients_require_mod = false
+server_filter_tags = { "announcement", "discord" }
+configuration_options = {}
+
+--https://forums.kleientertainment.com/forums/topic/124039-more-mod-config-options-options/
---
+++ modmain.lua
@@ -0,0 +1,195 @@
+
+
+local GetNewDeathAnnouncementString = GLOBAL.GetNewDeathAnnouncementString
+local GetNewRezAnnouncementString = GLOBAL.GetNewRezAnnouncementString
+local COMMAND_PERMISSION = GLOBAL.COMMAND_PERMISSION
+local GetGenderStrings = GLOBAL.GetGenderStrings
+local TheWorld = GLOBAL.TheWorld
+local TheSim = GLOBAL.TheSim
+local STRINGS = GLOBAL.STRINGS
+local json = GLOBAL.json
+local url = nil
+
+local character_icons = {
+    unknown = "https://cdn.discordapp.com/attachments/242336026251493376/735280247464788028/avatar_unknown.png",
+    charlie = "https://media.discordapp.net/attachments/381234050632908801/735364619551637646/avatar_charlie.png",
+    wagstaff = "https://cdn.discordapp.com/attachments/735356544174260255/735358342112870430/avatar_wagstaff.png",
+    walani = "https://cdn.discordapp.com/attachments/242336026251493376/735280248593186927/avatar_walani.png",
+    walter = "https://cdn.discordapp.com/attachments/242336026251493376/735280250539343902/avatar_walter.png",
+    warly = "https://cdn.discordapp.com/attachments/242336026251493376/735280252397289552/avatar_warly.png",
+    waxwell = "https://cdn.discordapp.com/attachments/242336026251493376/735280253974478858/avatar_waxwell.png",
+    wearger = "https://cdn.discordapp.com/attachments/242336026251493376/735280255673172018/avatar_wearger.png",
+    webber = "https://cdn.discordapp.com/attachments/242336026251493376/735280257107755078/avatar_webber.png",
+    weerclops = "https://cdn.discordapp.com/attachments/242336026251493376/735280258617573386/avatar_weerclops.png",
+    wendy = "https://cdn.discordapp.com/attachments/242336026251493376/735280259863281775/avatar_wendy.png",
+    wes = "https://cdn.discordapp.com/attachments/242336026251493376/735280260903469166/avatar_wes.png",
+    wheeler = "https://cdn.discordapp.com/attachments/735356544174260255/735358338656763965/avatar_wheeler.png",
+    wickerbottom = "https://cdn.discordapp.com/attachments/242336026251493376/735280261994119218/avatar_wickerbottom.png",
+    wathgrithr = "https://cdn.discordapp.com/attachments/242336026251493376/735280263139033108/avatar_wigfrid.png",
+    wilba = "https://cdn.discordapp.com/attachments/735356544174260255/735358331748745266/avatar_wilba.png",
+    wilbur = "https://cdn.discordapp.com/attachments/242336026251493376/735280264405843988/avatar_wilbur.png",
+    willow = "https://cdn.discordapp.com/attachments/242336026251493376/735280365790429194/avatar_willow.png",
+    wilson = "https://cdn.discordapp.com/attachments/242336026251493376/735280368583966810/avatar_wilson.png",
+    winona = "https://cdn.discordapp.com/attachments/242336026251493376/735280371343556678/avatar_winona.png",
+    wolfgang = "https://cdn.discordapp.com/attachments/242336026251493376/735280374325837974/avatar_wolfgang.png",
+    woodie = "https://cdn.discordapp.com/attachments/242336026251493376/735280376796282981/avatar_woodie.png",
+    woodlegs = "https://cdn.discordapp.com/attachments/242336026251493376/735280379270922310/avatar_woodlegs.png",
+    woose = "https://cdn.discordapp.com/attachments/242336026251493376/735280382332633179/avatar_woose.png",
+    wormot = "https://media.discordapp.net/attachments/381234050632908801/735363977118482442/avatar_wormot.png",
+    wormwood = "https://cdn.discordapp.com/attachments/242336026251493376/735280384891289721/avatar_wormwood.png",
+    wortox = "https://cdn.discordapp.com/attachments/242336026251493376/735280387378380890/avatar_wortox.png",
+    wragonfly = "https://cdn.discordapp.com/attachments/242336026251493376/735280392768323654/avatar_wragonfly.png",
+    wurt = "https://cdn.discordapp.com/attachments/242336026251493376/735280434950176778/avatar_wurt.png",
+    wx78 = "https://cdn.discordapp.com/attachments/242336026251493376/735280438947610754/avatar_wx78.png"
+}
+
+TheSim:GetPersistentString("discord_webhook_url", function(res, content)
+	if res then
+		url = content
+	else
+		url = nil
+	end
+end)
+
+function TitleCase( first, rest )
+   return first:upper()..rest:lower()
+end
+
+local function SendMessage(inst, message)
+    if not url then
+        return
+    end
+
+    local gender = GetGenderStrings(inst.prefab)
+    message = message:gsub(inst:GetDisplayName(), ""):gsub("^%s*(.-)%s*$", "%1"):gsub("(%a)([%w_']*)", TitleCase)
+
+    TheSim:QueryServer(
+		url,
+		function(json, res, code)
+			print("Announced to Discord", url, json)
+		end,
+		"POST",
+		json.encode({
+			username = inst:GetDisplayName(),
+			content = message,
+            avatar_url = character_icons[inst.prefab] or character_icons.unknown
+		})
+	) 
+end
+
+-- player_common_extensions.lua
+
+local function OnPlayerDeath(inst, data)
+    if not inst:HasTag("player") then
+        return
+    end
+
+    inst.deathcause = data ~= nil and data.cause or "unknown"
+
+    if data == nil or data.afflicter == nil then
+        inst.deathpkname = nil
+    elseif data.afflicter.overridepkname ~= nil then
+        inst.deathpkname = data.afflicter.overridepkname
+        inst.deathbypet = data.afflicter.overridepkpet
+    else
+        local killer = data.afflicter.components.follower ~= nil and data.afflicter.components.follower:GetLeader() or nil
+        if killer ~= nil and
+            killer.components.petleash ~= nil and
+            killer.components.petleash:IsPet(data.afflicter) then
+            inst.deathbypet = true
+        else
+            killer = data.afflicter
+        end
+        inst.deathpkname = killer:HasTag("player") and killer:GetDisplayName() or nil
+    end
+    
+    local announcement = GetNewDeathAnnouncementString(inst, inst.deathcause, inst.deathpkname, inst.deathbypet)
+    SendMessage(inst, announcement)
+end
+
+local function OnRespawnFromGhost(inst, data)
+    if inst.rezsource ~= nil then
+        local announcement = GetNewRezAnnouncementString(inst, inst.rezsource)
+        SendMessage(inst, announcement)
+    end
+end
+
+local function OnPlayerEntered(inst)
+    local announcement = string.format(STRINGS.UI.NOTIFICATION.JOINEDGAME, "")
+    SendMessage(inst, announcement)
+end
+
+local function OnPlayerExited(inst)
+    local announcement = string.format(STRINGS.UI.NOTIFICATION.LEFTGAME, "")
+    SendMessage(inst, announcement)
+end
+
+
+AddPrefabPostInit("world", function(world)
+    if not world.ismastersim then
+		return
+	end
+
+    world:ListenForEvent("ms_playerjoined", OnPlayerEntered)
+    world:ListenForEvent("ms_playerleft", OnPlayerExited)
+end)
+
+AddPlayerPostInit(function(inst)
+    inst:ListenForEvent("death", OnPlayerDeath)
+    inst:ListenForEvent("respawnfromghost", OnRespawnFromGhost)
+end)
+
+GLOBAL.SetDiscordWebhook = function(_url)
+    url = _url
+    TheSim:SetPersistentString("discord_webhook_url", url, false, nil)
+    print("set webhook")
+end
+
+-- AddUserCommand("webhook", {
+--     aliases = { "discordwebhook", "discord" },
+--     prettyname = "Set Discord Webhook",
+--     desc = "Set Discord webhook URL for this server, url should begin with \"https://discordapp.com/api/webhooks/\"",
+--     permission = COMMAND_PERMISSION.MODERATOR,
+--     confirm = false,
+--     slash = true,
+--     usermenu = false,
+--     servermenu = false,
+--     params = {"channel_id", "token"},
+--     vote = false,
+--     serverfn = function(param, caller)
+--         url = "https://discordapp.com/api/webhooks/" .. param.channel_id .. "/" .. param.token
+--         TheSim:SetPersistentString("discord_webhook_url", url, false, nil)
+--         print("set webhook")
+--     end,
+-- })
+
+
+
+-- makeplayerghost
+-- respawnfromghost
+-- ghostdissipated
+-- respawnfromcorpse
+-- playerdied
+-- gotnewitem
+-- killed (pvp)
+
+-- TheWorld:
+-- ms_playerjoined
+-- playerentered
+-- ms_playerleft
+-- playerexited
+-- ms_newplayerspawned
+-- deactivateworld
+-- ms_save
+-- deactivateworld
+-- ms_simunpaused
+-- ms_playerdisconnected
+-- ms_playerdespawn
+-- ms_worldreset
+-- ms_clientdisconnected
+-- entity_death
+-- ms_newplayercharacterspawned
+-- playeractivated
+-- playerdeactivated
+-- ms_setseason
+-- ms_save
